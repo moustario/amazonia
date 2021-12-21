a22bcc7b0470d3a6911a7372e287919fc3d7477a
moustario <muresanantoine@gmail.com>
2021-12-22 00:47:03 +0100

    feat(home): added wallet balance when purchasing

--- Controller/HomePage.php
+++ Controller/HomePage.php
@@ -24,8 +24,46 @@
 
         public function buy_gig($data)
         {
-            // TODO Check amount before
-            // TODO substract price to account and add to seller
+            // check if the buyer as enough to buy
+            $this->data["users"] = $this->db->get_users();
+            $this->data["gigs"] = $this->db->get_gigs();
+            $buyer = [];
+            $seller = [];
+            $product = [];
+            foreach ($this->data["users"] as $key => $user) {
+                if ($data["id_user"] == $user["id_user"]) {
+                    $buyer = $user;
+                }
+            }
+            foreach ($this->data["gigs"] as $key => $gig) {
+                if ($data["id_gig"] == $gig["id_gig"]) {
+
+                    $product = $gig;
+                }
+            }
+            foreach ($this->data["users"] as $key => $user) {
+                if ($product["id_user"] == $user["id_user"]) {
+                    $seller = $user;
+                }
+            }
+            if ($product["price"] > $buyer["wallet"]) {
+                return " | Not enough money in wallet, <br> currently have " . $buyer["wallet"] . "€";
+            }
+
+            // substracting from buyer
+            $updated_buyer = $buyer;
+            $updated_buyer["wallet"] = $updated_buyer["wallet"] - $product["price"];
+            $updated_buyer["type"] = "update";
+            $updated_buyer["id"] = $updated_buyer["id_user"];
+            $this->db->post_user($updated_buyer);
+
+            // adding to seller
+            $updated_seller = $seller;
+            $updated_seller["wallet"] = $updated_seller["wallet"] + $product["price"];
+            $updated_seller["type"] = "update";
+            $updated_seller["id"] = $updated_seller["id_user"];
+            $this->db->post_user($updated_seller);
+
             return $this->db->post_buy($data);
         }
     }
